22c7c4de3c4e07d09d585e467a1abf3f87762426
Ionut <nitescucornelionut19@gmail.com>
2021-04-08 15:10:58 +0300

    Backend

--- Backend/src/main/java/licenta/backend/service/RoomReservationService.java
+++ Backend/src/main/java/licenta/backend/service/RoomReservationService.java
@@ -1,7 +1,5 @@
 package licenta.backend.service;
 
-import licenta.backend.helpers.RoomDetail;
-import licenta.backend.helpers.RoomDetails;
 import licenta.backend.model.RoomReservation;
 import licenta.backend.repository.RoomReservationRepository;
 ;
@@ -9,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 @Service
