257d4e6414b9dec37753de874485dd854b975371
nites <nitescucornelionut19@gmail.com>
2021-05-25 14:43:59 +0300

    Reservation finished

---
+++ Backend/src/main/java/licenta/backend/helpers/ImageHelper.java
@@ -0,0 +1,13 @@
+package licenta.backend.helpers;
+
+public class ImageHelper {
+    public  String[] imagepath;
+
+    public String[] getImagepath() {
+        return imagepath;
+    }
+
+    public void setImagepath(String [] imagepath) {
+        this.imagepath = imagepath;
+    }
+}
