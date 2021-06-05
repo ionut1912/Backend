ebc80050edbadd76e1af80f106bb07e187e00773
nites <nitescucornelionut19@gmail.com>
2021-06-05 16:44:53 +0300

    RoomNrOfViews finished

---
+++ Backend/src/main/java/licenta/backend/helpers/RoomViewedHelper.java
@@ -0,0 +1,22 @@
+package licenta.backend.helpers;
+
+public class RoomViewedHelper {
+    public  long roomid;
+    public  long userid;
+
+    public long getRoomid() {
+        return roomid;
+    }
+
+    public void setRoomid(long roomid) {
+        this.roomid = roomid;
+    }
+
+    public long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(long userid) {
+        this.userid = userid;
+    }
+}
